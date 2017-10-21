4f982899247b979d90d47a9093901a336abc9003
Peter Somerville <peterwsomerville@gmail.com>
2017-10-21 13:33:37 +0100

    randomising

--- jkpic.py
+++ jkpic.py
@@ -7,6 +7,8 @@ tweeter = twitter.Api(consumer_key='RbIQCGFG2DUeCMeByYnOqwQE3',
                   access_token_key='921528056841416706-pza5BXDuC3rAQhjA3M6fMnoCpsvSjjd',
                   access_token_secret='OQRA49dgEjL6R5rZJaNzzndY07XdEaBaT177pGA3b9D22')
 
+zero_quotes = ['zero drops given', 'why does it never rain on jason?']
+
 files = glob.glob('img/*.jpg')
 with open('data/data.txt', 'rw') as f:
 	num_raindrops = int(f.read())
@@ -16,10 +18,13 @@ with open('data/data.txt', 'rw') as f:
 		s = 's'
 	out = '{} raindrop{} counted'.format(num_raindrops, s)
 	counted = random.randint(0, 100)
+	if (counted % 6 == 0):
+		counted = 0
 	num_raindrops += counted
 	if counted == 0:
-		out += ' (zero drops given)'
+		out += ' ({})'.format(random.choice(zero_quotes))
 	status = tweeter.PostUpdate(out, random.choice(files))
+	#print out
 
 with open('data/data.txt', 'w') as f:
 	f.write(str(num_raindrops))
