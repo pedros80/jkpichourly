d51927f4723306fb18b12bedaf85c0d8d56a07c6
Peter Somerville <peterwsomerville@gmail.com>
2017-10-21 12:03:20 +0100

    start dev

---
+++ test.py
@@ -0,0 +1,10 @@
+import twitter
+api = twitter.Api(consumer_key='RbIQCGFG2DUeCMeByYnOqwQE3',
+                  consumer_secret='AnN2klrw9HE628aDWAtPqZowgGD2Dr3s5EJgbUqzcH3VUNPeDN',
+                  access_token_key='921528056841416706-pza5BXDuC3rAQhjA3M6fMnoCpsvSjjd',
+                  access_token_secret='OQRA49dgEjL6R5rZJaNzzndY07XdEaBaT177pGA3b9D22')
+
+my_file = '/home/pedros/jkpic/spock.png'
+
+status = api.PostUpdate('', my_file)
+print(status)
